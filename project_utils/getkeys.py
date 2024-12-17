1a428f022b4eb7c74644a62a4c587484eef61531
Balqees Khalifa Khamis Al-Jabri <bxa936@student.bham.ac.uk>
2024-12-17 12:05:36 +0400

    feat: add getkeys function in project_utils for environment variable setup

---
+++ project_utils/getkeys.py
@@ -0,0 +1,8 @@
+import os
+import yaml 
+
+def getkeys(file_path):
+    with open(file_path, 'r') as file:
+        config = yaml.safe_load(file)
+        for key, value in config.items():
+            os.environ[key] = value
